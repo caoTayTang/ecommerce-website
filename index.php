98706e74c3c4b1d579fc64ec8c8aa937edcbb0a8
lechi <lechidaitp@gmail.com>
2022-07-05 23:30:57 +0700

    create interface for normal users

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>
+        Kaios
+    </title>
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+    <link rel="icon" href="./resource/logo.png">
+</head>
+<body>
+    <div id="main_div">
+        <?php include './menu.php' ?>
+        <?php include './header.php' ?>
+        <div id="middle_div">
+
+        </div>
+        <?php include './footer.php' ?>
+
+       
+    </div> 
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,25 @@
+<div id="menu">
+	<div id="category">
+		<ul>
+			<li>
+				<button>Hot</button>
+			</li>
+			<li>
+				<button>Đồ bình thường</button>
+			</li>
+			<li>
+				<button>Đầm</button>
+			</li>
+			<li>
+				<button>Giày dép</button>
+			</li>
+			<li>
+				<button>Đồ thể thao</button>
+			</li>
+			<li>
+				<button>Phụ kiện</button>
+			</li>
+		</ul>
+	</div>
+
+</div>
