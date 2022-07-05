8be6da713184d8c36f39bc7bf8bccc84e4d6c539
lechi <lechidaitp@gmail.com>
2022-07-06 04:20:00 +0700

    Move to Partitial

---
+++ Partial/footer.php
@@ -0,0 +1,52 @@
+<div id="footer">
+	<div id="contact">
+		<img src="./resource/logo-black.png" height="90px">
+	</div>
+	<div id="info">
+		<h2>Thông tin</h2>
+		<ul>
+			<li>
+				<a href="">Giúp đỡ</a>
+			</li>
+			<li>
+				<a href="">Theo dõi người dùng</a>
+			</li>
+			<li>
+				<a href="">Giao hàng và đổi trả</a>
+			</li>
+		</ul>
+	</div>
+	<div id="about">
+		<h2>Về Kaios</h2>
+		<ul>
+			<li>
+				<a href="">Kaios the fox</a>
+			</li>
+			<li>
+				<a href="">Làm việc cho chúng tôi</a>
+			</li>
+			<li>
+				<a href="">Nhân sự</a>
+			</li>
+		</ul>
+	</div>
+	<div id="more">
+		<h2>Ưu đãi</h2>
+		<ul>
+			<li>
+				<a href="">Voucher</a>
+			</li>
+			<li>
+				<a href="">Ngày hội giảm giá</a>
+			</li>
+			<li>
+				<a href="">Mobile app</a>
+			</li>
+		</ul>
+	</div>
+
+	<div id="bottom">
+		<div id="left">&nbsp&nbsp© 2022 Kaios</div>
+		<div id="right">Vietnam, Ho Chi Minh city &nbsp&nbsp</div>
+	</div>
+</div>
