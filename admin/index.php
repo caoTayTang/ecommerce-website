a253758d01c21001dd1b834620e356ff0251cec3
lechi <lechidaitp@gmail.com>
2022-07-06 08:01:32 +0700

    Update form

--- admin/index.php
+++ admin/index.php
@@ -11,20 +11,7 @@
 	<div id="main_div">
 		<?php include '../Partial/header.php'; ?>
 	
-	<div id="menu">
-		<div class="dropdown">
-	        <button class="dropbtn">Quản lý sản phẩm</button>
-	        <div class="dropdown-content">
-	            <a href="./products/form_insert.php">Thêm sản phẩm</a>
-	        </div>
-	    </div>
-	    <div class="dropdown">
-	        <button class="dropbtn">Quản lý  nhà sản xuất</button>
-	        <div class="dropdown-content">
-	            <a href="./manufacturers/form_insert.php">Thêm nhà sản xuất</a>
-	        </div>
-	    </div>
-	</div>
+        <?php include './menu.php'?>
 
   
 	 	<div id="middle_div" style="z-index: 9999;color: red;">
