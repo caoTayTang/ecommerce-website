a150ceebe435d714bec6b451eb662b35d6b1f8fc
lechi <lechidaitp@gmail.com>
2022-07-06 08:01:11 +0700

    Devide files into 2 sub-files for concise

---
+++ admin/menu.php
@@ -0,0 +1,14 @@
+<div id="menu">
+	<div class="dropdown">
+        <button class="dropbtn">Quản lý sản phẩm</button>
+        <div class="dropdown-content">
+            <a href="./products/form_insert.php">Thêm sản phẩm</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Quản lý  nhà sản xuất</button>
+        <div class="dropdown-content">
+            <a href="./manufacturers/form_insert.php">Thêm nhà sản xuất</a>
+        </div>
+    </div>
+</div>
