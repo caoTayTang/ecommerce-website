f41fafcb2e8b5719d7c34a2225f3384c2b77b214
lechi <lechidaitp@gmail.com>
2022-07-07 13:49:35 +0700

    Change href to the right link

--- admin/menu.php
+++ admin/menu.php
@@ -2,13 +2,13 @@
 	<div class="dropdown">
         <button class="dropbtn">Quản lý sản phẩm</button>
         <div class="dropdown-content">
-            <a href="./products/form_insert.php">Thêm sản phẩm</a>
+            <a href="/ecommerce-website/admin/products/form_insert.php">Thêm sản phẩm</a>
         </div>
     </div>
     <div class="dropdown">
         <button class="dropbtn">Quản lý  nhà sản xuất</button>
         <div class="dropdown-content">
-            <a href="./manufacturers/form_insert.php">Thêm nhà sản xuất</a>
+            <a href="/ecommerce-website/admin/manufacturers/form_insert.php">Thêm nhà sản xuất</a>
         </div>
     </div>
 </div>
