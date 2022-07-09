6941a6b4c0b13e9ffd5f8374599f85a999003ba6
lechi <lechidaitp@gmail.com>
2022-07-09 16:16:43 +0700

    style(admin + normal user): update pagination

--- Partial/pagination.php
+++ Partial/pagination.php
@@ -1,13 +1,55 @@
 <div id="pagination">
     <?php 
-        $concatChar = '?';
+        $concatChar = '?page=';
         if ( isset($_GET['query']) ) {
-            $concatChar = "?query=$search&";
+            $concatChar = "?query=$search" . "&page=";
         }
      ?>
-    <?php for($i = 1; $i <= $page; $i++) { ?>
-        <a href="index.php<?php echo $concatChar ?>page=<?php echo $i ?>"> 
+
+    <a href="index.php<?php echo $concatChar ?>1" name='quotation'> 
+        &laquo;
+    </a>  
+    <!-- $currentPage is from process_search.php -->
+    <?php if ($currentPage == 1) {
+        $pointer_event = "style='pointer-events: none;'";
+    }else $pointer_event = ""; ?>
+    <a href="index.php<?php echo $concatChar; echo $currentPage-1 ?>" <?php echo $pointer_event ?>> 
+        &lt;
+    </a>    
+
+<!-- Calculate the begin and end for the loop to show the correct page each case -->
+   <?php 
+   if ($currentPage == 1) {
+        $begin = $currentPage;
+        $end = $currentPage+2;
+   } else if ($currentPage == $page) {
+        $begin = $currentPage-2;
+        $end = $currentPage;
+   } else {
+        $begin = $currentPage-1;
+        $end = $currentPage+1;
+   }    
+    ?>
+
+    <?php 
+    $selected_style = "";
+    for($i = $begin; $i <= $end; $i++) { ?>
+        <?php if ($i == $currentPage ) $selected_style = "style='background-color: #000;color: white'"; ?>
+        <a href="index.php<?php echo $concatChar; echo $i ?>" <?php echo $selected_style ?>> 
             <?php echo $i ?>
         </a> 
+        <?php $selected_style = ""; ?>
     <?php } ?>
+
+
+    <?php if ($currentPage == $page) {
+        $pointer_event = "style='pointer-events: none;'";
+    }else $pointer_event = ""; ?>
+    <a href="index.php<?php echo $concatChar; echo $currentPage+1 ?>" <?php echo $pointer_event ?>> 
+        &gt;
+    </a>
+    <a href="index.php<?php echo $concatChar; echo $page?>"> 
+        &raquo;
+    </a> 
+
 </div>  
