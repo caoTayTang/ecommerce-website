f256db1c662a57f48ccdae2faf3cae9f9b5e610d
lechi <lechidaitp@gmail.com>
2022-07-13 18:13:30 +0700

    chore:  add newline

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="./styles.css">
     <link rel="icon" href="./resource/logo.png">
 </head>
-<body>
+<body>  
     <?php
     require './database/connect.php';
 
