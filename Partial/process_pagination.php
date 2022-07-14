8c2632570cb84b330d4a22314829d403387c0d40
lechi <lechidaitp@gmail.com>
2022-07-14 16:04:25 +0700

    feat: update pagination to new db (update query)

--- Partial/process_pagination.php
+++ Partial/process_pagination.php
@@ -14,8 +14,8 @@
         $search = addslashes($_GET['query']);
     }else $search = "";
     
-    $query = "select count(*) from products
-              where name like '%$search%'";
+    $query = "select count(*) from san_pham
+              where ten like '%$search%'";
 
     $total_products = mysqli_query($connect,$query);
     $total_products = mysqli_fetch_array($total_products)['count(*)'];
@@ -26,8 +26,17 @@
     $offset = $products_per_page*($currentPage-1);
 
      // Query the data that fits the ?query and ?page
-    $sql =  "select * from products
-            where name like '%$search%'
+    $sql =  "select 
+                san_pham.ma as ma,
+                san_pham.ten as ten,
+                san_pham.anh as anh,
+                san_pham.gia as gia,
+                the_loai.ten as the_loai
+            from san_pham
+            inner join the_loai_chi_tiet on san_pham.ma = the_loai_chi_tiet.ma_san_pham
+            inner join the_loai on the_loai_chi_tiet.ma_the_loai = the_loai.ma
+            where san_pham.ten like '%$search%'
             limit $offset,$products_per_page";
+
     $return = mysqli_query($connect,$sql);
 
