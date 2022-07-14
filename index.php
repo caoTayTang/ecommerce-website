1f927ec00ed44b2be2dc374f3cbd49ba54b9a432
lechi <lechidaitp@gmail.com>
2022-07-14 16:04:42 +0700

    feat: update to new db

--- index.php
+++ index.php
@@ -17,29 +17,29 @@
     require './partial/process_pagination.php';
     // Searching
     require './partial/process_search.php';
-
     ?>
     <div id="main_div">
             <?php 
             include './partial/menu.php';
-            include './partial/header.php'; ?>
+            include './partial/header.php'; 
+            ?>
             <div id="middle_div">
                 <div id="products_container">
                     <?php foreach($return as $each) { ?>
-                        <div class="each_product" onclick="location.href='/ecommerce-website/show.php?id=<?php echo $each['id'] ?>'">
-                            <div class="product_image" style="background-image: url(<?php echo $each['image'] ?>);">
+                        <div class="each_product" onclick="location.href='/ecommerce-website/show.php?id=<?php echo $each['ma'] ?>'">
+                            <div class="product_image" style="background-image: url(./admin/products/photos/<?php echo $each['anh'] ?>);">
                                 
                             </div>
                             <div class="product_name">
                                 <h2>
-                                    <?php echo $each['name'] ?>
+                                    <?php echo $each['ten'] ?>
                                 </h2>
                             </div>
                             <div class="product_price">
-                                <?php echo $each['price'] ?>
+                                <?php echo $each['gia'] ?>
                             </div>
                             <div class="product_tag">
-                                <?php echo $each['tag'] ?>
+                                <?php echo $each['the_loai'] ?>
                             </div>
                         </div>
                     <?php } ?>  
@@ -47,6 +47,6 @@
             </div>
             <?php include './partial/footer.php';?>
     </div> 
-    <?php mysqli_close($connect) ?>
+    <!-- <?php mysqli_close($connect) ?> -->
 </body>
 </html>
