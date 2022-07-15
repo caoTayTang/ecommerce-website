a84cff062086c314f9dee119cff33b1c04fec30a
lechi <lechidaitp@gmail.com>
2022-07-15 14:31:29 +0700

    chore: import resources

--- signing/process_sign_up.php
+++ signing/process_sign_up.php
@@ -1,14 +1,25 @@
 <?php 
 	session_start();
-	include '../database/connect.php';
 	$email = $_POST['email'];
 	$ten = $_POST['ten'];
 	$mat_khau = $_POST['mat_khau'];
 	$so_dien_thoai = $_POST['so_dien_thoai'];
-	$anh_dai_dien = $_POST['anh_dai_dien'];
+	$anh_dai_dien = $_FILES['anh_dai_dien'];
 
-	$query = "insert into khach_hang (email,ten,so_dien_thoai,mat_khau,anh_dai_dien)
-				values('$email','$ten','$so_dien_thoai','$mat_khau','$anh_dai_dien')";
+	//move image from temp to photos folder so that my local sever can select that later on
+	$folder = 'photos/'; 
+	//get img extension
+	$splitNameToArray = explode(".",$anh_dai_dien['name']); 
+	$arrayLength = count($splitNameToArray);
+	$file_extension = $splitNameToArray[$arrayLength-1];
+	$file_name = time() . '.' . $file_extension;
+	$path_file = $folder . $file_name;
+	move_uploaded_file($anh_dai_dien["tmp_name"],$path_file);
+
+	require '../database/connect.php';
+	$query = "insert into khach_hang
+			(email,ten,so_dien_thoai,mat_khau,anh_dai_dien)
+			values('$email','$ten','$so_dien_thoai','$mat_khau','$file_name')";
 	mysqli_query($connect,$query);
 	$error = mysqli_error($connect);
 	if ($error) {
@@ -16,7 +27,7 @@
 	}else {
 		$_SESSION['ma'] = mysqli_insert_id($connect);
 		$_SESSION['ten'] = $ten;
-		$_SESSION['anh_dai_dien'] = $anh_dai_dien; 
+		$_SESSION['anh_dai_dien'] = $file_name; 
 	}
 	
 	
---
+++ signing/user.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<?php include '../admin/form.php'?>
+	<link rel="stylesheet" type="text/css" href="../styles.css">
+	<link rel="icon" href="../resource/logo.png">
+	<style type="text/css">
+		/*Overriding some style*/
+		a[href='form_sign_up.php'] {
+			float: right;
+			font-size: 16px;
+			text-decoration: none;
+		}
+	</style>
+</head>
+<body>
+	<div id="main_div">
+		<?php 
+		include '../partial/menu.php';
+        include '../partial/header.php'; 
+        ?>
+        <div id="middle_div">
+        	<div class="login-box">
+				<h2>Xin chào <?php echo $_SESSION['ten'] ?></h2>
+				<a href="process_sign_out.php" style="margin-left: 40%;text-decoration: none;">
+						Đăng xuất
+				</a>
+			</div>
+        </div>
+    	<?php include '../partial/footer.php'; ?>
+	</div>
+</body>
+</html>
