bbe9ea25f6fe78840da40c70c04797d8d6044d8a
lechi <lechidaitp@gmail.com>
2022-07-19 17:42:48 +0700

    feat: implement signing for admin

--- admin/index.php
+++ admin/index.php
@@ -3,139 +3,56 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Nhà sản xuất</title>
+	<title></title>
+	<?php include '../admin/form.php'?>
 	<link rel="stylesheet" type="text/css" href="../styles.css">
-    <style type="text/css">
-        
-        select[name='sort_div'] {
-			padding: 3px 0;
-			color: black;
-            background-color: white;
-			margin-top: 20px;
-			border: 1px solid black;
-			border-radius: 3px;
-			outline: none;
-		}
-    </style>
 	<link rel="icon" href="../resource/logo.png">
+	<style type="text/css">
+		/*Overriding some style*/
+		a[href='form_sign_up.php'] {
+			float: right;
+			font-size: 16px;
+			text-decoration: none;
+		}
+		a[href="javascript:{}"] {
+			margin-left: 39% !important;
+		}
+	</style>
 </head>
 <body>
-    <?php
-    require '../database/connect.php';
-
-    //3 rows, 4 products each row
-    $products_per_page = 15;
-    require '../partial/process_pagination.php';
-    $num_rows = $return->num_rows;
-
-    // Searching
-    require '../partial/process_search.php';
-
-    $query_the_loai = "select * from the_loai";
-    $the_loai = mysqli_query($connect,$query_the_loai);
-    ?>
 	<div id="main_div">
-        
-		<?php include '../partial/header.php'; ?>
-        <?php include './menu.php'?>
+		<?php 
+		include '../partial/menu.php';
+        include '../partial/header.php';
 
-  
-	 	<div id="middle_div" style="z-index: 9999">
-            <div id="sort_div">
-                <form id="my_form" method="get" action="process_sort.php">
-                    <span style="color:black">Sắp xếp theo:</span>
-                    <select name="sort_div" onchange="document.getElementById('my_form').submit();">
-                    <option value="" selected disabled hidden>
-                        <?php
-                            if(isset($_GET['sort']) || !empty($_GET['sort'])) {
-                                 $sort = $_GET['sort'];
-                                 foreach($the_loai as $each) {
-                                     if($each['ma'] == $sort) {
-                                         echo $each['ten'];
-                                     } else if($sort == 0){
-                                         echo 'Lượt xem';
-                                         break;
-                                     } 
-                                 }
-                            }
-                        ?>
 
-                    </option>
-                        <option value="0">
-                            Lượt xem
-                        </option>
-                        <?php foreach($the_loai as $each) {  ?>
-                        <option value="<?php echo $each['ma'] ?>">
-                            <?php echo $each['ten'] ?>  
-                        </option> 
-                        <?php } ?>
-                    </select> 
-                </form>
-            </div>
-			<div id="content">
-                <p>
-                    <table id="main_table">
-                        <tr style="background-color: #95c5ff;">
-                            <th>
-                                Mã
-                            </th>
-                            <th>
-                                Tên
-                            </th>
-                            <th>
-                                Giá
-                            </th>
-                            <th>
-                                Ảnh
-                            </th>
-                            <th>
-                                Tên nhà sản xuất
-                            </th>
-                            <th>
-                                Sửa
-                            </th>
-                            <th>
-                                Xoá
-                            </th>
-                        </tr>
+		if (isset($_SESSION['ten']) && !empty($_SESSION['ten'])) {
+			echo("<script>window.location.href='user.php'</script>");
+			exit;
+		}
+        ?>
 
-                            <?php foreach($return as $each) { ?>
-                                <?php 
-                                // the rows color is alternating bewtween #ffffff and #f3f3f3 
-                                if ($num_rows % 2 == 0) $row_color = "#f1f3fa";
-                                else if ($num_rows % 2 == 1) $row_color = "#ffffff";
-                                $num_rows--;
-                                ?>
-                            <tr name="each_row" style="background-color: <?php echo $row_color ?>;">
-                                <td>
-                                    <?php echo $each['ma']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $each['ten'] ?>
-                                </td>
-                                <td>
-                                    <?php echo $each['gia']; ?>
-                                </td>
-                                <td>
-                                    <img src="./products/photos/<?php echo $each['anh'] ?>" height="100px" />
-                                </td>
-                                <td>
-                                    <?php echo $each['ten_nha_san_xuat']; ?>
-                                </td>
-                                <td class="update" onclick="location.href='products/form_update.php'">
-                                    Sửa
-                                </td>
-                                <td class="delete" onclick="location.href='products/process_delete.php'">
-                                    Xoá
-                                </td>   
-                            </tr>
-                            <?php } ?>
-                    </table>
-                </p>
-            </div>
-		</div>
-		<?php include '../partial/footer.php';?>
+
+        <div id="middle_div">
+        	<div class="login-box">
+				<h2>Đăng nhập admin</h2>
+				<form action="process_sign_in.php" method="post" id="my_form" enctype="multipart/form-data">
+		            <div class="user-box">
+                    Email<input type="email" name="email" required="">
+					</div>
+		            <div class="user-box">
+						Mật khẩu<input type="password" name="mat_khau" required>
+					</div>
+					<a href="javascript:{}" onclick="document.getElementById('my_form').submit();">
+						Đăng nhập
+					</a>
+					<a href="form_sign_up.php" style="margin-top: 15px;">
+						Đăng ký
+					</a>
+				</form>
+			</div>
+        </div>
+    	<?php include '../partial/footer.php'; ?>
 	</div>
-<?php mysqli_close($connect); ?>
 </body>
 </html>
---
+++ admin/process_sign_in.php
@@ -0,0 +1,40 @@
+<?php 
+	include '../database/connect.php';
+	$email = $_POST['email'];
+	$mat_khau = $_POST['mat_khau'];
+    if (isset($_POST['remember'])) {
+        $remember = true;
+    }else $remember = false;
+	
+	$query = "select * from nhan_vien 
+			  where email = '$email' AND mat_khau = '$mat_khau'";
+	$return = mysqli_query($connect,$query);
+	$number_rows = mysqli_num_rows($return);
+
+	if ($number_rows == 1) {
+		session_start();
+		$each = mysqli_fetch_array($return);
+		$_SESSION['ma'] = $each['ma'];
+		$_SESSION['ten'] = $each['ten'];
+		$_SESSION['anh_dai_dien'] = $each['anh_dai_dien'];
+        $_SESSION['cap_do'] = $each['cap_do'];
+
+        // if($remember) {
+        //     $id = $each['ma'];
+        //     do {
+        //         $token = uniqid('user_',true);
+        //         $sql = "update khach_hang
+        //                 set token = '$token'
+        //                 where ma = '$id'";
+        //         mysqli_query($connect,$sql);
+        //     } while(mysqli_error($connect));
+        //     setcookie('remember',$token,time() + 60*60*24*30); // set cookies if user checked remember me, expired after 1 month
+        // }
+
+		header('location: ./main_menu.php');
+		exit;
+	}else {
+		echo "<script>alert('Thất bại')</script>";
+	}
+
+	mysqli_close($connect);
