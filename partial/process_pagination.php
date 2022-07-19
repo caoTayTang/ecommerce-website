f48b20e31c35be6b4a74a4056d8a9941b607b2d1
lechi <lechidaitp@gmail.com>
2022-07-19 13:19:48 +0700

    fix: adjust query

--- partial/process_pagination.php
+++ partial/process_pagination.php
@@ -41,26 +41,21 @@
 
      // Query the data that fits the ?query and ?page
     $sql =  "select 
-                san_pham.ma as ma,
-                san_pham.ten as ten,
+                san_pham.ma as ma, san_pham.ten as ten,
                 san_pham.anh as anh,
                 san_pham.gia as gia,
-                the_loai.ten as the_loai,
-                the_loai.ma as ma_the_loai,
                 san_pham.so_luot_truy_cap as so_luot_truy_cap,
-                nha_san_xuat.ten as ten_nha_san_xuat 
-            from san_pham
+                group_concat(the_loai.ma) as ma_the_loai,
+                group_concat(the_loai.ten) as the_loai
+            from san_pham 
 
-            inner join the_loai_chi_tiet 
-            on san_pham.ma = the_loai_chi_tiet.ma_san_pham
-
-            inner join the_loai 
-            on the_loai_chi_tiet.ma_the_loai = the_loai.ma
-
-            inner join nha_san_xuat 
-           on san_pham.ma_nha_san_xuat = nha_san_xuat.ma
+            left join the_loai_chi_tiet 
+            on san_pham.ma = the_loai_chi_tiet.ma_san_pham 
+            left join the_loai 
+            on the_loai_chi_tiet.ma_the_loai = the_loai.ma 
 
             where (san_pham.ten like '%$search%' or mo_ta like '%$search%')
+            group by san_pham.ma
             $order
             limit $offset,$products_per_page";
     $return = mysqli_query($connect,$sql);
