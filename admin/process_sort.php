a46fccdd4c0fbe2b401c84b4f2a15c964f233a9f
lechi <lechidaitp@gmail.com>
2022-07-19 17:43:20 +0700

    feat: add sorting for admin

--- admin/process_sort.php
+++ admin/process_sort.php
@@ -2,9 +2,9 @@
 
 $sort = $_GET['sort_div'];
 if (isset($sort) || !empty($sort)) {
-header("location: ./index.php?sort=$sort");
+header("location: ./main_menu.php?sort=$sort");
 exit;
 }else {
-    header('location: ./index.php');
+    header('location: ./main_menu.php');
     exit;
 }
