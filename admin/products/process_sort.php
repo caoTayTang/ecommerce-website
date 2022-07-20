4908566e7525367bcad665cbf2ef0ebbf4e68606
lechi <lechidaitp@gmail.com>
2022-07-20 14:46:52 +0700

    refactor: move process sort to products\ and change link

--- admin/products/process_sort.php
+++ admin/products/process_sort.php
@@ -2,9 +2,9 @@
 
 $sort = $_GET['sort_div'];
 if (isset($sort) || !empty($sort)) {
-header("location: ./main_menu.php?sort=$sort");
+header("location: ./index.php?sort=$sort");
 exit;
 }else {
-    header('location: ./main_menu.php');
+    header('location: ./index.php');
     exit;
 }
