4a76ee68a8dacfdbe253cd2677296d39c031b1f1
lechi <lechidaitp@gmail.com>
2022-07-20 14:42:05 +0700

    feat: implement update products

--- admin/products/form_update.php
+++ admin/products/form_update.php
@@ -15,32 +15,48 @@
                if (!isset($_SESSION['cap_do'])) {
                 echo('<script>location.href="../index.php"</script>');
                 exit;
-            } 
+                } 
+               $ma = $_GET['ma'];
+               require '../../database/connect.php';
+               $sql = "select * 
+                        from san_pham
+                        where san_pham.ma = '$ma'";
+               $result = mysqli_fetch_array(mysqli_query($connect,$sql));
+
+            $query1 = "select * from nha_san_xuat";
+            $result1 = mysqli_query($connect,$query1);
         ?>
 
 	 	<div id="middle_div">
 	 		<div class="login-box">
 				<h2>Sửa sản phẩm</h2>
-				<form action="process_insert.php" method="post" id="my_form">
+				<form action="process_update.php" method="post" id="my_form" enctype="multipart/form-data">
+                    <input name='ma' value="<?=$result['ma']?>" hidden>
 					<div class="user-box">
-						<input type="text" name="name" required="">
-						<label>Tên sản phẩm</label>
+                    Tên sản phẩm<input type="text" name="ten" required="" value="<?=$result['ten']?>">
 					</div>
 					<div class="user-box">
-						<input type="number" name="price" required="">
-						<label>Giá tiền</label>
+					Giá tiền<input type="number" name="gia" required="" value="<?=$result['gia']?>">
 					</div>
 		            
-		            
 					<div class="user-box">
-		                <label>Ảnh</label>
-		                <br>
-		                <br>
-						<input type="file" name="image" required="">
+		                Ảnh
+						<input type="file" name="anh" required="">
 					</div>
 		            <div class="user-box">
-						<input type="text" name="manufacturer" required="">
-						<label>Tên nhà sản xuất</label>
+                        Nhà sản xuất:
+						<select name="ma_nha_san_xuat">
+							<?php foreach ($result1 as $each) { ?>
+                                <?php if ($each['ma'] == $result['ma_nha_san_xuat'] ) { ?>
+                                <option value="<?=$each['ma']?>" selected hidden>
+                                        <?php echo $each['ten']  ?>
+                                    </option>
+                                <?php } ?>
+								<option value="<?php echo $each['ma'] ?>">
+									<?php echo $each['ten']; ?>
+								</option>
+							<?php } ?>
+						</select>
 					</div>
 					<a href="javascript:{}" onclick="document.getElementById('my_form').submit();">
 						<span></span>
---
+++ admin/products/process_update.php
@@ -0,0 +1,38 @@
+<?php
+    $ma = $_POST['ma'];
+    $ten = $_POST['ten'];
+    $gia = $_POST['gia'];
+    $ma_nha_san_xuat = $_POST['ma_nha_san_xuat'];
+
+    if (!($_FILES['anh']['size'] == 0 || $_FILES['anh']['error'] == 4)) {
+        $anh = $_FILES['anh'];
+        //move image from temp to photos folder so that my local sever can select that later on 
+        $folder = 'photos/'; 
+        //get img extension
+        $splitNameToArray = explode(".",$anh['name']); 
+        $arrayLength = count($splitNameToArray);
+        $file_extension = $splitNameToArray[$arrayLength-1];
+        $file_name = time() . '.' . $file_extension;
+        $path_file = $folder . $file_name;
+        move_uploaded_file($anh["tmp_name"],$path_file);
+        $sub_query = " anh = '$file_name'";
+
+    }else $sub_query = "";
+
+    $query = "update san_pham set 
+                ten = '$ten',
+                gia = '$gia',
+                ma_nha_san_xuat = '$ma_nha_san_xuat',
+                $sub_query
+              where ma = '$ma'";
+    require '../../database/connect.php';
+	mysqli_query($connect,$query);
+
+	$error = mysqli_error($connect);
+	mysqli_close($connect);
+	if ($error) {
+		echo "<script>alert('Lỗi khi cập nhật, vui lòng làm lại!')</script>";
+    }else {
+        header('location: index.php');
+        exit;
+    }
