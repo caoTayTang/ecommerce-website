f4c05f1aa0f3598a2bad4009cec37cca02fa6b12
lechi <lechidaitp@gmail.com>
2022-07-20 14:41:12 +0700

    feat: implement insert manufacturer

--- admin/manufacturers/form_insert.php
+++ admin/manufacturers/form_insert.php
@@ -11,8 +11,8 @@
 		<?php include '../../partial/header.php'; ?>
 	 	<?php include '../menu.php' ?>
         <?php 
-               if (empty($_SESSION['cap_do'])) { //empty = !isset + !0
-                echo('<script>location.href="../index.php"</script>');
+               if (empty($_SESSION['cap_do'])) { //empty = !isset && !0
+                echo('<script>location.href="../../index.php"</script>');
                 exit;
             } 
         ?>
@@ -21,25 +21,10 @@
 				<h2>Thêm nhà sản xuất</h2>
 				<form action="process_insert.php" method="post" id="my_form">
 					<div class="user-box">
-						<input type="text" name="name" required="">
-						<label>Tên nhà sản xuất</label>
-					</div>
-					<div class="user-box">
-						<input type="number" name="price" required="">
-						<label>Địa chỉ</label>
-					</div>
-					<div class="user-box">
-		                <label>Số điện thoại</label>
-		                <br>
-		                <br>
-						<input type="file" name="image" required="">
-					</div>
-		            <div class="user-box">
-						<input type="text" name="manufacturer" required="">
-						<label>Tên nhà sản xuất</label>
+                        Tên nhà sản xuất<input type="text" name="ten" required="">
 					</div>
 					<a href="javascript:{}" onclick="document.getElementById('my_form').submit();">
-						Sửa
+                        Thêm
 					</a>
 				</form>
 			</div>
---
+++ admin/manufacturers/process_insert.php
@@ -0,0 +1,16 @@
+<?php 
+    if(empty($_POST['ten'])) {
+		echo "<script>alert('Xin điền đầy đủ thông tin!')</script>";
+        die();
+	}
+
+	$ten = $_POST['ten'];
+	require '../../database/connect.php';
+
+	$query = "insert into nha_san_xuat(ten)
+		values('$ten')";
+
+	mysqli_query($connect,$query);
+	mysqli_close($connect);
+    header('location: ../index.php');
+    exit;
