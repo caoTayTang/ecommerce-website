60813868fe4b0fcb8efdc316cd8f3db26214c264
lechi <lechidaitp@gmail.com>
2022-07-21 09:14:16 +0700

    feat: split menu for admin and super admin

---
+++ admin/products/menu.php
@@ -0,0 +1,10 @@
+<div id="menu">
+    <div id="cancel_button"></div>
+	<div class="dropdown">
+        <button class="dropbtn" onclick="location.href='./index.php'">Quản lý sản phẩm</button>
+        <div class="dropdown-content">
+            <a href="/ecommerce-website/admin/products/form_insert_type.php">Thêm thể loại</a>
+            <a href="/ecommerce-website/admin/products/form_insert.php">Thêm sản phẩm</a>
+        </div>
+    </div>
+</div>
