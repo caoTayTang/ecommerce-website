623811b0fb6735535a3f9d98333f3a18f53329ea
lechi <lechidaitp@gmail.com>
2022-07-21 09:15:19 +0700

    feat: add href to dropbtn

--- admin/menu.php
+++ admin/menu.php
@@ -1,14 +1,14 @@
 <div id="menu">
     <div id="cancel_button"></div>
 	<div class="dropdown">
-        <button class="dropbtn">Quản lý sản phẩm</button>
+        <button class="dropbtn" onclick="location.href='../products/index.php'">Quản lý sản phẩm</button>
         <div class="dropdown-content">
             <a href="/ecommerce-website/admin/products/form_insert_type.php">Thêm thể loại</a>
             <a href="/ecommerce-website/admin/products/form_insert.php">Thêm sản phẩm</a>
         </div>
     </div>
     <div class="dropdown">
-        <button class="dropbtn">Quản lý  nhà sản xuất</button>
+        <button class="dropbtn" onclick="location.href='../manufacturers/index.php'">Quản lý  nhà sản xuất</button>
         <div class="dropdown-content">
             <a href="/ecommerce-website/admin/manufacturers/form_insert.php">Thêm nhà sản xuất</a>
         </div>
