a71e52ab399addf5e68cf84c8986e208e476c1ea
lechi <lechidaitp@gmail.com>
2022-07-25 20:24:14 +0700

    feat: validate insert manufacturer

--- admin/manufacturers/form_insert.php
+++ admin/manufacturers/form_insert.php
@@ -10,8 +10,17 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Thêm nhà sản xuất</title>
 	<?php include '../form.php'?>
+	<style type="text/css">
+	 	.user-box {
+        	margin: 0;
+        }
+
+        input {
+        	margin-bottom: 5px!important;
+        }
+	</style>
 </head>
 <body>
 	<div id="main_div">
@@ -24,7 +33,8 @@ session_start();
 					<div class="user-box">
                         Tên nhà sản xuất<input type="text" name="ten" required="">
 					</div>
-					<a href="javascript:{}" onclick="document.getElementById('my_form').submit();">
+					<span class="error_span"></span>
+					<a href="javascript:{}" onclick="validate()">
                         Thêm
 					</a>
 				</form>
@@ -34,3 +44,35 @@ session_start();
 	</div>
 </body>
 </html>
+
+<script type="text/javascript">
+		//validating form
+        function validate() {
+			//input
+			const ten = document.querySelector("[name='ten']");
+			// Error span
+			const error_span = document.getElementsByClassName('error_span');
+
+			let isValid = true;
+
+			if( !check_name(ten) )
+			{
+				error_span[0].innerHTML = "Tên nhà sản không được để trống";
+			}else error_span[0].innerHTML = ""
+
+			if (isValid) {
+				document.getElementById('my_form').submit();
+			}else {
+				console.log("khong valid nhe");
+			}
+		}
+
+		//check name if its > 0 character and some regex
+		function check_name(name)
+		{
+			if (name.value == "") {
+				return false;
+			} else return true;
+		}
+
+</script>
