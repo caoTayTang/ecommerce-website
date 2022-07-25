b1bebc8f19499bc4c2b8caa01fa7af32d2c5bbd5
lechi <lechidaitp@gmail.com>
2022-07-25 12:21:25 +0700

    feat: make menu for new feature

--- admin/menu.php
+++ admin/menu.php
@@ -1,5 +1,16 @@
 <div id="menu">
     <div id="cancel_button"></div>
+
+    <div class="dropdown">
+        <button class="dropbtn" onclick="location.href='../orders'">Quản lý đơn hàng</button>
+        <div class="dropdown-content">
+            <a href="/admin/orders/pending_orders.php">Đơn chờ duyệt</a>
+            <a href="/admin/orders/approved_orders.php">Đơn đã duyệt</a>
+            <a href="/admin/orders/canceled_orders.php">Đơn đã huỷ</a>
+        </div>
+
+    </div>
+
 	<div class="dropdown">
         <button class="dropbtn" onclick="location.href='../products/index.php'">Quản lý sản phẩm</button>
         <div class="dropdown-content">
@@ -7,13 +18,16 @@
             <a href="/admin/products/form_insert.php">Thêm sản phẩm</a>
         </div>
     </div>
+
+    <div class="dropdown">
+        <button class="dropbtn" onclick="location.href='../orders'">Quản lý nhân viên</button>
+    </div>
+
     <div class="dropdown">
         <button class="dropbtn" onclick="location.href='../manufacturers/index.php'">Quản lý  nhà sản xuất</button>
         <div class="dropdown-content">
             <a href="/admin/manufacturers/form_insert.php">Thêm nhà sản xuất</a>
         </div>
     </div>
-    <div class="dropdown">
-        <button class="dropbtn" onclick="location.href='../orders'">Quản lý đơn hàng</button>
-    </div>
+
 </div>
