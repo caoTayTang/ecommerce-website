fd0e40da31e0c6889d1af0d1034654e3c2c467d7
lechi <lechidaitp@gmail.com>
2022-07-26 13:30:33 +0700

    refactor: change order of menu

--- admin/products/menu.php
+++ admin/products/menu.php
@@ -1,10 +1,20 @@
 <div id="menu">
     <div id="cancel_button"></div>
+    <div class="dropdown">
+        <button class="dropbtn" onclick="location.href='/admin/orders'">Quản lý đơn hàng</button>
+        <div class="dropdown-content">
+            <a href="/admin/orders/pending_orders.php">Đơn chờ duyệt</a>
+            <a href="/admin/orders/approved_orders.php">Đơn đã duyệt</a>
+            <a href="/admin/orders/canceled_orders.php">Đơn đã huỷ</a>
+    </div>
+
 	<div class="dropdown">
         <button class="dropbtn" onclick="location.href='./index.php'">Quản lý sản phẩm</button>
         <div class="dropdown-content">
             <a href="/admin/products/form_insert_type.php">Thêm thể loại</a>
             <a href="/admin/products/form_insert.php">Thêm sản phẩm</a>
         </div>
+    </div>
+
     </div>
 </div>
