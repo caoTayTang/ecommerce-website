b1f7c53726162749a13b2800c8bbed7220faef8e
lechi <lechidaitp@gmail.com>
2022-07-26 17:09:08 +0700

    vendor: update database

--- ecommerce-website.sql
+++ ecommerce-website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 20, 2022 at 01:46 PM
+-- Generation Time: Jul 26, 2022 at 10:07 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -47,8 +47,12 @@ INSERT INTO `hoa_don` (`ma`, `ma_khach_hang`, `trang_thai`, `dia_chi_nguoi_nhan`
 (19, 11, 1, '123 Time City', '2022-07-20 11:46:09', 'Kaios đệ nhị', '030402053533', '43gqer', 340),
 (20, 11, 1, 'Số 8 đường số 7', '2022-07-20 12:04:02', 'Mua áo ma mị', '05992042', 'Không ghi chú', 1393938),
 (21, 11, 1, '12324232', '2022-07-20 12:05:46', 'Lại mua hàng', '12342332', '321232312', 3000),
-(22, 11, 1, 'nhanh', '2022-07-20 12:07:21', 'nhanh', '129', 'nahn', 24690),
-(23, 11, 1, 'Làng Vũ Đại', '2022-07-20 12:11:57', 'Kaios the fox', '0987654', 'không ghi chú', 278690);
+(23, 11, 1, 'Làng Vũ Đại', '2022-07-20 12:11:57', 'Kaios the fox', '0987654', 'không ghi chú', 278690),
+(24, 11, 1, 'time city', '2022-07-25 04:14:19', 'Không biết ghi tên gì', '01010101', '123', 2090907),
+(25, 11, 2, 'time city', '2022-07-25 04:15:05', 'Không biết ghi tên gì', '01010101', 'ghi chu nay', 24690),
+(26, 11, 1, 'time city', '2022-07-25 05:05:48', 'Không biết ghi tên gì', '01010101', 'lai mua thu', 696969),
+(27, 18, 0, 'Dia chi trong tim em', '2022-07-25 08:09:53', 'Đại đẹp trai', '0363259454', 'đây là mua 3 cái kính nè', 1500),
+(28, 11, 0, 'time city', '2022-07-26 01:02:40', 'Không biết ghi tên gì', '01010101', '', 27036);
 
 -- --------------------------------------------------------
 
@@ -73,7 +77,12 @@ INSERT INTO `hoa_don_chi_tiet` (`ma_hoa_don`, `ma_san_pham`, `so_luong`, `gia`) 
 (21, 12, 3, 1000),
 (23, 2, 1, 250000),
 (23, 12, 4, 4000),
-(23, 13, 2, 24690);
+(23, 13, 2, 24690),
+(24, 9, 3, 2090907),
+(25, 13, 2, 24690),
+(26, 9, 1, 696969),
+(27, 42, 3, 1500),
+(28, 36, 3, 27036);
 
 -- --------------------------------------------------------
 
@@ -97,9 +106,8 @@ CREATE TABLE `khach_hang` (
 --
 
 INSERT INTO `khach_hang` (`ma`, `anh_dai_dien`, `ten`, `so_dien_thoai`, `dia_chi`, `email`, `mat_khau`, `token`) VALUES
-(11, '1658236888.jpg', 'Không phải Kaios', '01010101', 'time city', 'abc@abc', '111', 'user_62d7eee00b3203.50514874'),
-(12, '1657870100.jpg', 'tên', '111', 'Vin home', 'email', 'pass', 'user_62d3b9f4adc7b2.73158025'),
-(13, '1657969358.png', 'Đại đẹp trai', '0303030303', 'Tim em', 'daideptrai@gmail.com', '123', 'user_62d7f3f2798fc8.45888417');
+(11, '1658367769.png', 'Không biết ghi tên gì', '01010101', 'time city', 'abc@abc.abc', '12345678ABCdai#', 'user_62dfb9c0368f01.01239417'),
+(18, '1658377884.jpg', 'Đại đẹp trai', '0363259454', 'Dia chi trong tim em', 'ledai3062005@gmail.com', '123', NULL);
 
 -- --------------------------------------------------------
 
@@ -125,7 +133,10 @@ CREATE TABLE `nhan_vien` (
 
 INSERT INTO `nhan_vien` (`ma`, `anh_dai_dien`, `ten`, `so_dien_thoai`, `dia_chi`, `ngay_sinh`, `email`, `mat_khau`, `cap_do`) VALUES
 (1, 'default.png', 'admin', '010101010101', 'HCM', '2022-07-03', 'admin@gmail.com', '123', b'0'),
-(2, 'default.png', 'super Admin', '111', 'Ha Noi', '2022-07-03', 'superadmin@gmail.com', '123', b'1');
+(2, 'default.png', 'super Admin', '111', 'Ha Noi', '2022-07-03', 'superadmin@gmail.com', '123', b'1'),
+(5, '1658732434.jpg', 'Admin 2', '0920298103', 'Hồ Chí Minh city', '2022-07-18', 'admin2@gmail.com', '123', b'0'),
+(6, '1658732619.jpg', 'Admin 3', '0908070605', 'Day la dia chi', '2022-07-17', 'admin3@gmail.com', '123', b'0'),
+(7, '1658732699.png', 'Super admin 2', '8923890238', 'day la dia chi cua superadmin2', '2022-02-10', 'superadmin2@gmail.com', '123', b'1');
 
 -- --------------------------------------------------------
 
@@ -168,26 +179,16 @@ CREATE TABLE `san_pham` (
 --
 
 INSERT INTO `san_pham` (`ma`, `ten`, `mo_ta`, `anh`, `gia`, `ma_nha_san_xuat`, `so_luot_truy_cap`) VALUES
-(2, 'Áo dài óng ánh', 'Áo Dài Eo Việt từ lâu đã trở thành thương hiệu áo dài đẳng cấp mà người Việt tin dùng.', '1657785713.jpg', 250000, 2, 213),
-(8, 'Áo đỏ nhưng không có được em', 'anamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplash', '1658148045.jpg', 340, 2, 2),
-(9, 'Ma mị nhưng vẫn luỵ', 'abhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplash', '1658302526.jpg', 696969, 1, 5),
-(10, 'Đồng hồ', 'clay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplash', '1658148114.jpg', 99999, 1, 3),
-(11, 'Túi xách', 'kakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\n', '1658148137.jpg', 0, 2, 0),
+(2, 'Áo dài óng ánh', 'Áo Dài Eo Việt từ lâu đã trở thành thương hiệu áo dài đẳng cấp mà người Việt tin dùng.', '1657785713.jpg', 250000, 2, 215),
+(8, 'Áo đỏ nhưng không có được em', 'anamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplashanamnesis33-6IfIKXJ_bx8-unsplash', '1658148045.jpg', 340, 2, 20),
+(9, 'Ma mị nhưng vẫn luỵ', 'abhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplashabhijeet-gourav-6hXOKaeYw-o-unsplash', '1658302526.jpg', 696969, 1, 15),
+(10, 'Đồng hồ', 'clay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplashclay-banks-kAOL6GOJDPE-unsplash', '1658148114.jpg', 99999, 1, 14),
+(11, 'Túi xách', 'kakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\n', '1658148137.jpg', 0, 2, 3),
 (12, 'Áo vàng áo xanh làm chứng cho anh', 'kakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\nkakaman vai\r\n', '1658302751.jpg', 1000, 2, 6),
-(13, 'Miệng cười banh mà lòng như chó rách', ':(', '1658148278.jpg', 12345, 3, 3),
-(14, 'Tone xanh vì nhớ anh', ':(', '1658148313.jpg', 192489, 2, 2),
-(15, 'Nhảy đương đại do ngây dại', ':(', '1658148356.jpg', 9012, 2, 0),
-(16, 'Nhảy đương đại do ngây dại', ':(', '1658149838.jpg', 9012, 2, 1),
-(17, 'Nhảy đương đại do ngây dại', ':(', '1658149841.jpg', 9012, 2, 1),
-(26, 'Nhảy đương đại do ngây dại', ':(', '1658149854.jpg', 9012, 2, 1),
-(27, 'Nhảy đương đại do ngây dại', ':(', '1658149855.jpg', 9012, 2, 0),
-(28, 'Nhảy đương đại do ngây dại', ':(', '1658149856.jpg', 9012, 2, 0),
-(34, 'Nhảy đương đại do ngây dại', ':(', '1658149864.jpg', 9012, 2, 1),
-(35, 'Nhảy đương đại do ngây dại', ':(', '1658149865.jpg', 9012, 2, 0),
-(36, 'Nhảy đương đại do ngây dại', ':(', '1658149866.jpg', 9012, 2, 0),
-(40, 'Website SEO dở thật ', 'Con cá đối nằm trên cối đá', '1658209644.jpg', 500, 2, 0),
-(41, 'Website SEO dở thật ', 'Con cá đối nằm trên cối đá', '1658210215.jpg', 500, 2, 0),
-(42, 'Website SEO dở thật ', 'Con cá đối nằm trên cối đá', '1658210221.jpg', 500, 2, 6);
+(13, 'Miệng cười banh mà lòng như chó rách', ':(', '1658148278.jpg', 12345, 3, 4),
+(14, 'Tone xanh vì nhớ anh', ':(', '1658148313.jpg', 192489, 2, 27),
+(36, 'Nhảy đương đại do ngây dại', ':(', '1658149866.jpg', 9012, 2, 2),
+(42, 'Website SEO dở thật ', 'Con cá đối nằm trên cối đá', '1658210221.jpg', 500, 2, 24);
 
 -- --------------------------------------------------------
 
@@ -207,7 +208,13 @@ CREATE TABLE `san_pham_da_luu` (
 
 INSERT INTO `san_pham_da_luu` (`ma_khach_hang`, `ma_san_pham`, `thoi_gian`) VALUES
 (11, 1, '2022-07-16 12:01:56'),
-(11, 2, '2022-07-16 12:01:53');
+(11, 2, '2022-07-16 12:01:53'),
+(11, 8, '2022-07-26 17:00:24'),
+(11, 9, '2022-07-21 08:34:27'),
+(11, 10, '2022-07-26 16:58:23'),
+(11, 11, '2022-07-26 17:00:44'),
+(11, 14, '2022-07-26 16:56:54'),
+(11, 42, '2022-07-26 14:27:41');
 
 -- --------------------------------------------------------
 
@@ -231,7 +238,8 @@ INSERT INTO `the_loai` (`ma`, `ten`) VALUES
 (4, 'Phụ kiện'),
 (5, 'Đồ ngủ'),
 (6, 'Đồ cưới'),
-(7, 'ABC');
+(7, 'ABC'),
+(8, 'DEF');
 
 -- --------------------------------------------------------
 
@@ -259,6 +267,7 @@ INSERT INTO `the_loai_chi_tiet` (`ma_san_pham`, `ma_the_loai`) VALUES
 (42, 3),
 (10, 4),
 (42, 4),
+(57, 4),
 (14, 5),
 (15, 5),
 (16, 5),
@@ -360,19 +369,19 @@ ALTER TABLE `the_loai_chi_tiet`
 -- AUTO_INCREMENT for table `hoa_don`
 --
 ALTER TABLE `hoa_don`
-  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `khach_hang`
 --
 ALTER TABLE `khach_hang`
-  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `nhan_vien`
 --
 ALTER TABLE `nhan_vien`
-  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `nha_san_xuat`
@@ -390,7 +399,7 @@ ALTER TABLE `san_pham`
 -- AUTO_INCREMENT for table `the_loai`
 --
 ALTER TABLE `the_loai`
-  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
