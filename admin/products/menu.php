4f2522c96f97077d8b83895ecc37f90be1206921
lechi <lechidaitp@gmail.com>
2022-07-27 19:22:12 +0700

    refactor: add enter

--- admin/products/menu.php
+++ admin/products/menu.php
@@ -1,11 +1,14 @@
 <div id="menu">
     <div id="cancel_button"></div>
+
     <div class="dropdown">
         <button class="dropbtn" onclick="location.href='/admin/orders'">Quản lý đơn hàng</button>
         <div class="dropdown-content">
             <a href="/admin/orders/pending_orders.php">Đơn chờ duyệt</a>
             <a href="/admin/orders/approved_orders.php">Đơn đã duyệt</a>
             <a href="/admin/orders/canceled_orders.php">Đơn đã huỷ</a>
+        </div>
+        
     </div>
 
 	<div class="dropdown">
