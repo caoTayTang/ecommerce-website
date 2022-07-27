8d4b4d196bcda39778ec84d32f25095a5449cc75
lechi <lechidaitp@gmail.com>
2022-07-27 19:20:39 +0700

    feat: make new header, user UI for admin

---
+++ admin/header.php
@@ -0,0 +1,69 @@
+<?php
+// session already started at another files
+if(isset($_SESSION['cap_do']) ) {
+    if (!isset($_SESSION['anh_dai_dien'])) {
+        $anh_dai_dien = 'default.png';
+    }else {
+        $anh_dai_dien = $_SESSION['anh_dai_dien'];
+    }
+}
+?>
+<div id="overlay"></div>
+<div id="header">
+    <div id="hamburger" onclick="logo(this)">
+        <div id="hamburger_container">
+              <div class="bar1"></div>
+              <div class="bar2"></div>
+              <div class="bar3"></div>
+        </div>
+    </div>
+    <div id="logo">
+    <a href="/admin/index.php" >
+            <img src="/resource/logo-white.png" >
+        </a>
+    </div>
+    <div id="search_bar">
+        <form>
+<?php if (!isset($search)) 
+$search = "";
+// Prevent undefine when include header file in other index.php
+?>
+            <input type="text" placeholder="Tìm kiếm" name="query" value="<?php echo stripslashes($search) ?>">
+        </form>
+    </div>
+    <div id="user" onclick="location.href='/admin/user.php'" style="
+    background-image: url('/admin/employees/photos/<?php echo $anh_dai_dien ?>');
+    background-size:30px;
+    background-position: 15px 20px;
+    background-repeat: no-repeat;">
+    </div>
+        <div id="shopping_cart" onclick="alert('Bạn là admin!')"></div>
+    <div id="help" onclick="location.href='https://github.com/caoTayTang/ecommerce-website'"></div>
+</div>
+<script>
+
+function logo(x) {
+    let toggleBar = document.getElementById('menu');
+    let menu = document.getElementById('hamburger_container');
+    let overlay = document.getElementById('overlay');
+    let cancel = document.getElementById('cancel_button')
+
+        toggleBar.style.display = "block";
+    menu.style.position = "fixed";
+    overlay.style.display = "block";
+
+    // If cancle div or the gray overlay be clicked
+    cancel.addEventListener("click",() => {
+    toggleBar.style.display = "none";
+    menu.style.position = "relative";
+    overlay.style.display = "none";
+        })
+
+        overlay.addEventListener("click", () => {
+        toggleBar.style.display = "none";
+        menu.style.position = "relative";
+        overlay.style.display = "none";
+        });
+    }
+
+</script>
---
+++ admin/user.php
@@ -0,0 +1,68 @@
+<?php 
+session_start(); 
+if (!isset($_SESSION['ma']) || empty($_SESSION['ma'])) {
+    header('location:./form_sign_in.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<?php include '../admin/form.php'?>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/hack-font@3/build/web/hack-subset.css">
+	<link rel="stylesheet" type="text/css" href="../styles.css">
+	<link rel="icon" href="../resource/logo.png">
+	<style type="text/css">
+		/*Overriding some style*/
+		a[href='form_sign_up.php'] {
+			float: right;
+			font-size: 16px;
+			text-decoration: none;
+		}
+        
+        .action {
+            text-decoration: none;
+            font-size: 20px;
+            color: #0784d9; 
+        }
+        
+        .login-box {
+            padding: 20px
+        }
+
+        h2 {
+            margin-bottom: 0px !important;
+        }
+	</style>
+</head>
+<body>
+	<div id="main_div">
+		<?php 
+		if ($_SESSION['cap_do'] == 1)
+            include './menu.php';
+        else 
+            include './products/menu.php';
+
+        include './header.php'; 
+        ?>
+        <div id="middle_div">
+        	<div class="login-box">
+				<h2>Xin chào <?php echo $_SESSION['ten'] ?></h2>
+				<a class="action" href="/signing/process_sign_out.php">
+                    <span style="font-family:Hack, monospace;font-size:25px">&nbsp</span>
+                    Đăng xuất
+				</a>
+				<br>
+				<a class="action" href="/admin/employees/form_update_profile.php">
+                    <span style="font-family:hack, monospace;font-size:20px">&nbsp</span>
+                    Cập nhật hồ sơ
+				</a>
+			</div>
+        </div>
+    	<?php include '../partial/footer.php'; ?>
+	</div>
+</body>
+</html>
