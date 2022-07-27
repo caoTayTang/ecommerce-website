87d36dc28f3c94a51a22ab4cb82a567c6d0e1918
lechi <lechidaitp@gmail.com>
2022-07-27 19:20:18 +0700

    feat: update profile for admin

---
+++ admin/employees/form_update_profile.php
@@ -0,0 +1,167 @@
+<?php
+session_start();
+if (!isset($_SESSION['ten']) || empty($_SESSION['ten'])) {
+    header('location: user.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<?php include '../../admin/form.php'?>
+	<link rel="stylesheet" type="text/css" href="../../styles.css">
+	<link rel="icon" href="../../resource/logo.png">
+	<style type="text/css">
+		/*Overwriting some styles*/
+		.login-box form a[href='form_sign_in.php'] {
+			float: right;
+			font-size: 16px;
+			text-decoration: none;
+		}
+	  	.user-box {
+	    	margin: 0;
+	    }
+
+	    input {
+	    	margin-bottom: 10px!important;
+	    }
+	</style>
+</head>
+<body>
+	<div id="main_div">
+		<?php 
+		if ($_SESSION['cap_do'] == 1)
+            include '../menu.php';
+        else 
+            include '../products/menu.php';
+        include '../header.php'; ?>
+        <?php 
+            require '../../database/connect.php';
+            $ma = $_SESSION['ma'];
+            // if admin -> CRUD at table admin
+            if (isset($_SESSION['cap_do'])) { 
+                $table = "nhan_vien";
+            } else $table = "khach_hang";
+            $query = "select * from $table where ma = '$ma'";
+            $result = mysqli_fetch_array(mysqli_query($connect,$query));
+        ?>
+        <div id="middle_div">
+        	<div class="login-box">
+				<h2>Cập nhật hồ sơ</h2>
+				<form action="process_update_profile.php" method="post" id="my_form" enctype="multipart/form-data">
+                    <input type="text" hidden name='ma' value="<?php echo $ma ?>">
+		            <div class="user-box">
+                    Email<input type="email" name="email" required="" value="<?php echo $result['email'] ?>">
+					</div>
+					<span class="error_span"></span>
+
+					<div class="user-box">
+					Tên<input type="text" name="ten" required value="<?php echo $result['ten'] ?>">
+					</div>
+					<span class="error_span"></span>
+					
+					<div class="user-box">
+						Mật khẩu<input type="password" name="mat_khau" required>
+					</div>
+					<span class="error_span"></span>
+
+					<div class="user-box">
+						Số điện thoại<input type="text" name="so_dien_thoai" required value="<?php echo $result['so_dien_thoai'] ?>">
+					</div>
+					<span class="error_span"></span>
+					
+					<div class="user-box">
+		                Ảnh đại diện<input type="file" name="anh_dai_dien">
+					</div>
+					
+					<a href="javascript:{}" onclick="validate();" style="margin-left: 40%;">
+                        Xác nhận
+					</a>
+				</form>
+			</div>
+        </div>
+    	<?php include '../../partial/footer.php'; ?>
+	</div>
+</body>
+</html>
+
+
+<script type="text/javascript">
+		//validating form
+        function validate() {
+			//input
+			const email = document.querySelector("[type='email']");
+			const ten = document.querySelector("[name='ten']");
+			const mat_khau = document.querySelector("[name='mat_khau']");
+			const so_dien_thoai = document.querySelector("[name='so_dien_thoai']");
+			// Error span
+			const error_span = document.getElementsByClassName('error_span');
+
+			let isValid = true;
+
+			if( !check_email(email) )
+			{
+				error_span[0].innerHTML = "Email không hợp lệ";
+				isValid = false;
+			} else error_span[0].innerHTML = "";
+
+			if( !check_name(ten) )
+			{
+				error_span[1].innerHTML = "Tên không hợp lệ";
+				isValid = false;
+			} else error_span[1].innerHTML = "";
+
+			if( !check_password(mat_khau) )
+			{
+				error_span[2].innerHTML = "Mật khẩu phải có ít nhất 8 kí tự, <br> có chữ in hoa, chữ thường, có kí tự đặc biệt";
+				isValid = false;
+			} else error_span[2].innerHTML = "";
+
+
+			if( !check_phone_number(so_dien_thoai) )
+			{
+				error_span[3].innerHTML = "Số điện thoại không hợp lệ!";
+				isValid = false;
+			} else error_span[3].innerHTML = "";
+
+			if (isValid) {
+				document.getElementById('my_form').submit();
+			}else {
+				console.log("khong valid nhe");
+			}
+		}
+
+		function check_email(email)
+		{
+			const regexEmail = /^\w+([\.-]?\w+)*\@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+			return regexEmail.test(email.value)
+		}
+
+		//At least 8 chars, 1 special num, 1 capital, 1 normal and no number required
+		function check_password(password)
+		{
+			const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+			return regexPassword.test(password.value);
+		}		
+
+		//check name if its > 0 character and some regex
+		function check_name(name)
+		{
+			//Họ tên: (có thể có 1 chữ cái, dấu hay không đều được)
+			const regexName = /^([A-VXYỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ]{0,}[a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ ,.'-]?)+$/; 
+			return regexName.test(name.value);
+		}	
+
+		//at least 8 number, do not have ^00...
+		function check_phone_number(phone_number)
+		{
+			const regexPassword = /0?[1-9]+[0-9]{7,}/
+			return regexPassword.test(phone_number.value);
+		}		
+
+
+</script>
+
---
+++ admin/employees/process_update_profile.php
@@ -0,0 +1,59 @@
+<?php 
+	session_start();
+    if (empty($_POST['ma']) || empty($_POST['email']) || empty($_POST['so_dien_thoai']) || empty($_POST['mat_khau']) ) {
+        echo "<script>alert('Xin điền đầy đủ thông tin')</script>";
+        die();
+    }
+
+    $ma = addslashes($_POST['ma']);
+	$email = addslashes($_POST['email']);
+	$ten = addslashes($_POST['ten']);
+	$so_dien_thoai = addslashes($_POST['so_dien_thoai']);
+    $mat_khau = addslashes($_POST['mat_khau']);
+
+    $file_name = $_SESSION['anh_dai_dien'];
+    if (!($_FILES['anh_dai_dien']['size'] == 0 || $_FILES['anh_dai_dien']['error'] == 4)) {
+        $anh_dai_dien = $_FILES['anh_dai_dien'];
+
+        if ($anh_dai_dien['size'] > 5000000) {
+          echo "<script>alert('Kích thước ảnh quá lớn (>5MB)')</script>";
+          die();
+        }
+        if ($anh_dai_dien['type'] != "image/png" && $anh_dai_dien['type'] != "image/jpeg") {
+          echo "<script>alert('Chỉ chấp nhận ảnh jpg/png')</script>";
+          die();
+        }
+
+        //move image from temp to photos folder so that my local sever can select that later on 
+        $folder = 'photos/'; 
+        //get img extension
+        $splitNameToArray = explode(".",$anh_dai_dien['name']); 
+        $arrayLength = count($splitNameToArray);
+        $file_extension = $splitNameToArray[$arrayLength-1];
+        $file_name = time() . '.' . $file_extension;
+        $path_file = $folder . $file_name;
+        move_uploaded_file($anh_dai_dien["tmp_name"],$path_file);
+    }
+    $query = "update nhan_vien set 
+                email = '$email',
+                ten = '$ten',
+                so_dien_thoai = '$so_dien_thoai',
+                anh_dai_dien = '$file_name',
+                mat_khau = '$mat_khau'
+              where ma = '$ma'";
+
+	require '../../database/connect.php';
+
+	mysqli_query($connect,$query);
+	$error = mysqli_error($connect);
+	if (!empty($error)) {
+		echo "<script>alert('Lỗi khi cập nhật, vui lòng làm lại!')</script>";
+        die();
+	}else {
+		$_SESSION['ma'] = $ma;
+		$_SESSION['ten'] = $ten;
+		$_SESSION['anh_dai_dien'] = $file_name; 
+	}
+	
+	mysqli_close($connect);
+    header('location: ../user.php');
