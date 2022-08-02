e423e827aa90600dec253f7b8be14d019695ac68
lechi <lechidaitp@gmail.com>
2022-08-02 17:30:13 +0700

    fix: validate if id is invalid

--- cart/update_quantity.php
+++ cart/update_quantity.php
@@ -5,13 +5,19 @@ try {
         throw new Exception('Thiếu id');
     }
     $id = $_GET['id'];
-    if ($_GET['type'] === 'remove') {
-        if ($_SESSION['cart']["$id"]['so_luong'] > 1) $_SESSION['cart']["$id"]['so_luong']--;
-        else unset($_SESSION['cart']["$id"]);
-    } else if($_GET['type'] === 'add') {
-        $_SESSION['cart']["$id"]['so_luong']++;
+    if ( !empty($_SESSION['cart']["$id"]) ) {
+        if ($_GET['type'] === 'remove') {
+            
+            if ($_SESSION['cart']["$id"]['so_luong'] > 1) $_SESSION['cart']["$id"]['so_luong']--;
+            else unset($_SESSION['cart']["$id"]);
+        
+        } else if($_GET['type'] === 'add') {
+            $_SESSION['cart']["$id"]['so_luong']++;
+        } else {
+            throw new Exception('Thiếu type');
+        }
     } else {
-        throw new Exception('Thiếu type');
+        throw new Exception('ID không hợp lệ');
     }
     echo 1;
 } catch (Throwable $e) {
